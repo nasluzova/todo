0d2a81f23c931322c60537aa4c2104e01f280907
Elizaveta <lisanasluzova@gmail.com>
2024-06-20 22:10:54 +0200

    display with '

--- main.js
+++ main.js
@@ -1,31 +1,79 @@
+const Status = {
+  todo: "To do",
+  inProgress: "In progress",
+  done: "Done",
+};
 const toDoList = {
   list: {
-    "create a new practice task": "In progress",
-    "make a bed": "Done",
+    "create a new practice task": "to do",
+    "make a bed": "In progress",
     "write a post": "to do",
+    "feed the cat": "Done",
   },
   changeStatus(task, status) {
-    for (task in toDoList.list) {
+    if (task in toDoList.list) {
       this.list[task] = status;
+    } else {
+      console.log("No such task");
     }
   },
+
   addTask(task) {
-    this.list[task] = "to do";
+    if (task in toDoList.list) {
+      return;
+    } else {
+      this.list[task] = "to do";
+    }
   },
   deleteTask(task) {
     delete toDoList.list[task];
   },
   showList() {
-    if (task in toDoList.list && toDoList.list[task] == "to do") {
-      console.log(task + ":" + toDoList.list[task]);
-      if (task in toDoList.list && toDoList.list[task] == "In progress") {
-        console.log(task + ":" + toDoList.list[task]);
+    let countToDo = 0;
+    for (let task in toDoList.list) {
+      if (toDoList.list[task] === "to do") {
+        countToDo++;
+        console.log("'" + task + "'" + ":" + "'" + toDoList.list[task] + "'");
       }
+    }
+    if (countToDo === 0) {
+      console.log("-");
+    }
+    let countInProgress = 0;
+    for (let task in toDoList.list) {
+      if (toDoList.list[task] === "In progress") {
+        countInProgress++;
+        console.log("'" + task + "'" + ":" + "'" + toDoList.list[task] + "'");
+      }
+    }
+    if (countInProgress === 0) {
+      console.log("-");
+    }
+
+    let countDone = 0;
+    for (let task in toDoList.list) {
+      if (toDoList.list[task] === "Done") {
+        countDone++;
+        console.log("'" + task + "'" + ":" + "'" + toDoList.list[task] + "'");
+      }
+    }
+    if (countDone === 0) {
+      console.log("-");
     }
   },
 };
+
+console.log(toDoList.list);
+function addTask(task, status) {
+  if (task in toDoList.list) {
+    console.log("В списке уже есть эта задача");
+    return;
+  } else {
+    toDoList.list[task] = status;
+  }
+}
+addTask("water plants", Status.inProgress);
+
+console.log(toDoList.list);
+addTask("water plants", Status.done);
 toDoList.showList();
-toDoList.addTask("Do the dishes");
-toDoList.addTask("Probe");
-toDoList.deleteTask("Probe");
-toDoList.changeStatus("make a bed", "to do");
